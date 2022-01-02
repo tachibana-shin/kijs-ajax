e94849596c30b581d9c19772b39507f90e4b9557
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2022-01-02 10:45:43 +0700

    tsc

--- src/kijs.d.ts
+++ src/kijs.d.ts
@@ -1,21 +1,24 @@
-import { support, Kijs } from "kijs";
+import { Kijs, support } from "kijs";
 
 declare module "kijs" {
-  interface support {
-    cors: boolean;
-    ajax: boolean;
-    createHTMLDocument: boolean;
-  }
+  const support: {
+    readonly cors: boolean;
+    readonly ajax: boolean;
+    readonly createHTMLDocument: boolean;
+  };
+
+  export { support }
 }
 
 declare module "kijs" {
-  interface Kijs {
-    load: (
+  class Kijs {
+    load(
       url: string,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       params?: any,
       callback?: Required<Options>["success"]
-    ) => Kijs;
-    [
+    ): this;
+    readonly [
       name:
         | "ajaxStart"
         | "ajaxStop"
--- src/setup/isSupport.ts
+++ src/setup/isSupport.ts
@@ -1,9 +1,14 @@
+import { support } from "kijs";
+
 import ajaxSettings from "../static/ajaxSettings";
 
 const xhrSupported = !!ajaxSettings.xhr();
 
+// eslint-disable-next-line functional/immutable-data
 support.cors = !!xhrSupported && "withCredentials" in xhrSupported;
+// eslint-disable-next-line functional/immutable-data
 support.ajax = xhrSupported;
+// eslint-disable-next-line functional/immutable-data
 support.createHTMLDocument = (() => {
   const body = document.implementation.createHTMLDocument("").body;
   body.innerHTML = "<form></form><form></form>";
